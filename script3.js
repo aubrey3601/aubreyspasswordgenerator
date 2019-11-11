b24ba2dc3dc43ecabcc178b8ee5d8e8716719dc0
Aubrey McCarthy <aubrey3601@gmail.com>
2019-11-10 22:45:46 -0700

    updated js & html

--- script3.js
+++ script3.js
@@ -13,20 +13,46 @@
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacters = ["!", "@", "#", "$", "%","^", "&", "*"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"];
 
 
 
 // Step 2: 
 // 1. create a function that asks the user about the specifics of the password:
 //     -length
-var pwdLength = 10;
 
-document.querySelector("btn btn-danger");
+var button = document.querySelector(".btn-danger");
+console.log(button)
+button.addEventListener("click", function () {
+    console.log("click me again");
+    var requestedLength = prompt("How long do you want yours password between 8-128 characters?");
+    var requestedLengthNumber = parseInt(requestedLength);
 
-document.addEventListener("click", function(){
-   
-})
+    //NEXT STEPS:
+    //would you like special characters? (set a variable for this)
+    //do you want upper case letters?
+    //would you like numbers?
+
+    console.log("requested lenght is " + requestedLengthNumber);
+    if (requestedLengthNumber >= 8 && requestedLengthNumber <= 128) {
+
+        var password = "";
+        // lowerAndUpp = lowerCase.concat(upperCase);
+        for (var i = 0; i < requestedLengthNumber; i++) {
+            var letter = lowerCase[parseInt(Math.random() * lowerCase.length)];
+            console.log("letter equals " + letter);
+            password = password + letter;
+        }
+        var outputpwd = document.querySelector(".outputpwd");
+        outputpwd.innerHTML = password;
+    } else {
+        alert("Please request a password between 8 & 128 characters long!");
+    }
+});
+
+
+
+
 
 //     -lowerCase characters
 //     -upperCase characters
@@ -34,7 +60,7 @@ document.addEventListener("click", function(){
 //     -special characters
 //     Save each of these specifications in a variable. Now we know password specifics!
 
-  
+
 // 2. once we know the password's specifics, we can now create another variable
 //     that will include ALL the characters that the user wants to include. For example, 
 //     if the user wants to include numbers and special characters only, this new variable will have ONLY 
@@ -44,7 +70,7 @@ document.addEventListener("click", function(){
 // 1. create another function that will take two parameters (length and specifiedCharacters), 
 //     and inside this function create a new variable and assign an empty array to it. 
 //     This new variable will hold your generated password. 
-    
+
 // 2. this function will also include your loop and push a randomly selected 
 //     character from specifiedCharacters into a new variable that you just created. So, how many time does this loop run?
 
